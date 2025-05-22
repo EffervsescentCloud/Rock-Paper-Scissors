d906140e5312cef6f94837952c789ed9134b76d0
GEOFFREY Fisher Seufert <7003549@cpsd.us>
2025-05-22 13:32:27 -0400

    added functionality for getting players choice

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+let choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random()*3);
-    let choices = ["rock", "paper", "scissors"];
     return choices[randomNumber];
+}
+function getHumanChoice() {
+    let humanChoice = prompt("Choose rock, paper, or scissors");
+    while (true) {
+        if (choices.includes(humanChoice)) {
+            return humanChoice;
+        }
+        else {
+            humanChoice = prompt("Choice invalid. Please choose between rock, paper, or scissors")
+        }
+    }
 }
 
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
